e5384d737647f3508384453ff95ada2c6661b904
Dark5888 <liu.mike588@gmail.com>
2025-07-23 23:38:13 -0400

    basic css formatting and basic navbar format

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import { Navbar } from './components/Navbar';
 import {Hero} from './components/Hero';
 import {Projects} from './components/Projects'
 import { Contact } from './components/Contact';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [isLoaded , setIsLoaded] = useState(false)
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, [])
   return (
-    <>
-      {" "}
+    <div className ={`app ${isLoaded ? "loaded" : ""}`}>
       <Navbar /> 
-      <p> HELLO WORLD WORKFLOW</p>
+
       <Hero />
       <Projects />
       <Contact />
@@ -19,7 +24,7 @@ function App() {
       <>
         <p> &copy; 2025 Michael Liu. All rights reserved.</p>
       </>
-    </>
+    </div>
   )
 }
 
